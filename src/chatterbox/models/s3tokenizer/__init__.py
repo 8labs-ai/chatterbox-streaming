8ea13b1435f687bd05146b14d385cc2b342a574d
OverFitted <kozak.bor@gmail.com>
2025-09-19 17:30:06 +0300

    Fix: coerce SOS/EOS positions to Python ints before slicing in drop_invalid_tokens

--- src/chatterbox/models/s3tokenizer/__init__.py
+++ src/chatterbox/models/s3tokenizer/__init__.py
@@ -16,15 +16,19 @@ EOS = SPEECH_VOCAB_SIZE + 1
 def drop_invalid_tokens(x):
     """Drop SoS and EoS"""
     assert len(x.shape) == 1 or (len(x.shape) == 2 and x.shape[0] == 1), "only batch size of one allowed for now"
-    if SOS in x:
-        s = (x == SOS).nonzero(as_tuple=True)[0].squeeze(0) + 1
+    if len(x.shape) == 2:
+        x = x.squeeze(0)
+
+    start_idxs = (x == SOS).nonzero(as_tuple=True)[0]
+    if start_idxs.numel() > 0:
+        s = int(start_idxs[0].item()) + 1
     else:
         s = 0
 
-    if EOS in x:
-        e = (x == EOS).nonzero(as_tuple=True)[0].squeeze(0)
+    end_idxs = (x == EOS).nonzero(as_tuple=True)[0]
+    if end_idxs.numel() > 0:
+        e = int(end_idxs[0].item())
     else:
         e = None
 
-    x = x[s: e]
-    return x
+    return x[s:e]
